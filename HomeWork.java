716798edb03904bb79770b95a317bbfba1f26bf5
evgenyKozhemyakin <90960795+evgenyKozhemyakin@users.noreply.github.com>
2021-12-22 22:35:11 +0300

    Add files via upload

--- HomeWork.java
+++ HomeWork.java
@@ -1,40 +1,45 @@
-Class HomeWork {
+/**
+*Java 1. HomeWork #1
+*
+* @autor Evgeny Kozhemyakin
+* @version 22.12.2021
+*/
+class HomeWork {
     public static void main(String[] args) {
         printThreeWords();
-        checkSumSign ();
-        printColor ();
-        compareNumbers ();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
-	
-    static void printThreeWords(){
+    static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    static void checkSumSign () {
-        int a = -30 ;
-        int b = 20 ;
-        System.out.println ((a+b >=0) ? "Sum is positiv" : "Sum is negativ") ;
+    static void checkSumSign() {
+        int a = -30;
+        int b = 20;
+        System.out.println((a + b >= 0) ? "Sum is positiv" : "Sum is negativ");
     }
-    static void printColor () {
-    int value =100 ;
-    if (value <=0) {
-    System.out.println("Red") ;
-        }
-        if (value > 0 || value <=100) {
-        System.out.println("Yellow") ;
-        }
-        else {
-        System.out.println("Green") ;
+    static void printColor() {
+        int value = 100;
+        if (value <= 0) {
+            System.out.println("Red");
+        } else if (value <= 100) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
         }
     }
-        public static void compareNumbers () {
+    static void compareNumbers() {
         int a = 12;
         int b = 22;
-        if (a >=b) {
+        if (a >= b) {
             System.out.println(" a >= b ");
+        }
         else {
-              System.out.println(" a < b ") ;
+            System.out.println(" a < b ");
         }
     }
+
 }
